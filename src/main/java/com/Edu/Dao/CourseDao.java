090bffb2dc5e2c2404255ce7eb6f44e08a299335
cotgyu <v123v123s@naver.com>
2018-02-20 15:59:22 +0900

    dao, course 부분 수정(실수로 확인안하고 올려서 다시 수정했습니다.)

--- src/main/java/com/Edu/Dao/CourseDao.java
+++ src/main/java/com/Edu/Dao/CourseDao.java
@@ -1,9 +1,8 @@
 package com.Edu.Dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Edu.Domain.Course;
 
-public interface CourseDao extends JpaRepository<Course, Integer> {
+public interface CourseDao {
 
 }
--- src/main/java/com/Edu/Domain/Course.java
+++ src/main/java/com/Edu/Domain/Course.java
@@ -1,19 +1,15 @@
 package com.Edu.Domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 import org.apache.ibatis.type.Alias;
 
 import lombok.Data;
 
-@Entity
+
 @Alias("course")
 public @Data class Course {
 	
-	@Id
-    @GeneratedValue
+	
 	private int cosno;	
 	private String cosname;
 	private String cosintro;
